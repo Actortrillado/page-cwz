84aa06cb7a3f652361146434cd116f8729ee28cd
Actortrillado <benjamin.puente@estudiantes.colegiotgs.cl>
2025-10-02 15:52:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ st.set_page_config(
     page_title="todo el lore de call of duty",
     page_icon=":no_entry:"
     ) 
-st.logo(":dark_sunglasses:")
+st.logo("call.png")
 
 
 pg = st.navigation(["Pagina principal.py", "Secretos.py", "el mayor fraude.py", "armas meta.py", "kesto.py", "quienes somos.py"])
 pg.run()
+
 
 
 
